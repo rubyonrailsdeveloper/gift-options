f47cbdb67f02bcda59715deb2101d1f8e0412caa
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2017-06-08 02:57:51 -0700

    shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "gift-options"
   config.api_key = "5c3e32ff8a8c43b9246c449e69cd7ec8"
   config.secret = "70938dcffd05ef85bf11d1a663902c17"
   config.scope = "write_orders, read_products"
