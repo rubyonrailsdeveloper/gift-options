207c9cd7902656409f192ed4ecfbe82d3a670cea
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2017-06-08 02:54:48 -0700

    shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "5c3e32ff8a8c43b9246c449e69cd7ec8"
   config.secret = "70938dcffd05ef85bf11d1a663902c17"
-  config.scope = "read_orders, read_products"
+  config.scope = "write_orders, read_products"
   config.embedded_app = true
 end
