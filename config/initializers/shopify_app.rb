1159af96240ee1ec101052f9712727544cb17d16
rubyonrailsdeveloper <webdeveloper1001@outlook.com>
2017-06-08 16:56:16 -0700

    update shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,8 @@
 ShopifyApp.configure do |config|
-  config.application_name = "gift-options"
-  config.api_key = "5abf721c91d3fe7c4e77ed668bf4ec35"
+  config.application_name = "My Shopify App"
+  config.api_key = "5c3e32ff8a8c43b9246c449e69cd7ec8"
   config.secret = "70938dcffd05ef85bf11d1a663902c17"
-  config.scope = "read_orders, read_products"
+  config.redirect_uri = "https://gift-options.herokuapp.com/auth/shopify/callback"
+  config.scope = "read_products"
   config.embedded_app = true
 end
